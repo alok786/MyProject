569f8aaa8e86d7d00074a000e72e21a0e34521a6
LAPTOP-KKMIOIO7\alok <kalok4410@gmail.com>
2021-06-03 19:20:55 +0530

    Initial Code

---
+++ myapp/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	fmt.Println("Execution started...")
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+	e.GET("/download", func(c echo.Context) error {
+		return c.File("/assets/image1")
+		//return c.String(http.StatusOK,"/assets/image1")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+	
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+  	// User ID from path `users/:id`
+  	id := c.Param("id")
+	//return c.String(http.StatusOK, id)
+	html := `<div><p>Name</p> <p>ID : `+id+`</p></div>`
+	return c.HTML(http.StatusOK, html)
+}
+
+//e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+// e.POST("/save", save)
+func saveUser(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
