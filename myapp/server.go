d6e552ad76090d98903488082a2d38180645bbfd
LAPTOP-KKMIOIO7\alok <kalok4410@gmail.com>
2021-06-04 12:52:55 +0530

    Modified Content

--- myapp/server.go
+++ myapp/server.go
@@ -14,7 +14,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, World!!!!!")
 	})
 	e.POST("/users", saveUser)
 	e.GET("/users/:id", getUser)
